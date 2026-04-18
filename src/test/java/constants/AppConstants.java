98ece2318e0ee125a9d8eda735271fbff279ad63
Rgl-RogueLike <nikita.harit@mail.ru>
2026-04-18 23:13:59 +0300

    feat(constants): add constants in AppConstants

--- src/test/java/constants/AppConstants.java
+++ src/test/java/constants/AppConstants.java
@@ -13,4 +13,8 @@ public class AppConstants {
     public static final String SORT_PRICE_HIGH_LOW = "Price High > Low";
     public static final int MIN_PRODUCTS_COUNT = 4;
     public static final int DEFAULT_TIMEOUT_SECONDS = 10;
+    public static final int MIN_QUANTITY_PRODUCTS = 1;
+    public static final int MAX_QUANTITY_PRODUCTS = 7;
+    public static final int INDEX_SECOND_PRODUCT = 1;
+    public static final int INDEX_THRID_PRODUCT = 2;
 }
