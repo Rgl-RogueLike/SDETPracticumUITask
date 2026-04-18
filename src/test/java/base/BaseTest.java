12be1c26e1b842531e6b12c3137f55d0529a0b8e
Rgl-RogueLike <nikita.harit@mail.ru>
2026-04-18 23:16:52 +0300

    fix(base): fix TimeoutException when loading the page (install pageLoadStrategy.EAGER)

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,9 +1,11 @@
 package base;
 
+import constants.AppConstants;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -23,10 +25,11 @@ public class BaseTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         options.addArguments("--remote-allow-origins=*");
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
 
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(AppConstants.DEFAULT_TIMEOUT_SECONDS));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(AppConstants.DEFAULT_TIMEOUT_SECONDS));
     }
 
     @AfterEach
