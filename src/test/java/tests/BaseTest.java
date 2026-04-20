101686092efc19cc057a766902c357b8010d135a
Rgl-RogueLike <nikita.harit@mail.ru>
2026-04-20 18:45:36 +0300

    refactor(core): update BaseTest to use ThreadLocal for WebDriver (parallel execution support)

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -11,18 +11,29 @@ import java.time.Duration;
 
 public abstract class BaseTest {
 
-    static WebDriver driver;
-    static WebDriverWait waiter;
+    private static final ThreadLocal<WebDriver> DRIVER = new ThreadLocal<>();
+    private static final ThreadLocal<WebDriverWait> WAITER = new ThreadLocal<>();
+
+    protected WebDriver driver;
+    protected WebDriverWait waiter;
 
     @BeforeEach
     public void setUp() {
-        driver = new ChromeDriver();
-        waiter = new WebDriverWait(driver, Duration.ofSeconds(Long.parseLong(ParameterProvider.get("explicit.wait.time"))));
+        DRIVER.set(new ChromeDriver());
+        WAITER.set(new WebDriverWait(DRIVER.get(),
+                Duration.ofSeconds(Long.parseLong(ParameterProvider.get("explicit.wait.time")))));
+        driver = DRIVER.get();
+        waiter = WAITER.get();
         driver.get(ParameterProvider.get("base.url"));
     }
 
     @AfterEach
     public void tearDown() {
-        driver.quit();
+        WebDriver drv = DRIVER.get();
+        if (drv != null) {
+            drv.quit();
+            DRIVER.remove();
+            WAITER.remove();
+        }
     }
 }
