a4c409f89a89a42fb101bb8d4bb89b215ccb6028
Rgl-RogueLike <nikita.harit@mail.ru>
2026-04-20 12:50:22 +0300

    refactor(pages): migrate MainPage to BasePage and simplify wait logic

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -6,15 +6,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import utils.WaitHelper;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainPage {
-
-    private WebDriver driver;
-    protected WaitHelper waitHelper;
+public class MainPage extends BasePage {
 
     @FindBy(id = "categorymenu")
     private WebElement categoryMenu;
@@ -22,20 +20,17 @@ public class MainPage {
     @FindBy(id = "filter_keyword")
     private WebElement searchInput;
 
-    public MainPage(WebDriver driver) {
-        this.driver = driver;
-        this.waitHelper = new WaitHelper(driver, 10);
+    @FindBy(xpath = "//section[@id='categorymenu']//ul[contains(@class, 'nav-pills categorymenu')]/li/a")
+    private List<WebElement> categoryLinks;
+
+    public MainPage(WebDriver driver, WebDriverWait waiter) {
+        super(driver, waiter);
         PageFactory.initElements(driver, this);
+        waiter.until(ExpectedConditions.visibilityOf(categoryMenu));
     }
 
 
     public List<String> getAvailableCategories() {
-        waitHelper.waitForVisibility(categoryMenu);
-
-        List<WebElement> categoryLinks = driver.findElements(
-                By.xpath("//section[@id='categorymenu']//ul[contains(@class, 'nav-pills categorymenu')]/li/a")
-        );
-
         List<String> categories = new ArrayList<>();
         for (WebElement link : categoryLinks) {
             categories.add(link.getText().trim());
@@ -45,15 +40,15 @@ public class MainPage {
 
     public CategoryPage navigateToCategory(String categoryName) {
         By linkLocator = By.linkText(categoryName);
-        WebElement categoryLink = waitHelper.waitForClickable(linkLocator);
+        WebElement categoryLink = waiter.until(ExpectedConditions.elementToBeClickable(linkLocator));
         categoryLink.click();
-        return new CategoryPage(driver);
+        return new CategoryPage(driver, waiter);
     }
 
     public ProductListingPage searchFor(String text) {
-        waitHelper.waitForVisibility(searchInput);
+        waiter.until(ExpectedConditions.visibilityOf(searchInput));
         searchInput.clear();
         searchInput.sendKeys(text + Keys.ENTER);
-        return new ProductListingPage(driver);
+        return new ProductListingPage(driver, waiter);
     }
 }
